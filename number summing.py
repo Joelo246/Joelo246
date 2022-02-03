772b586909af26ccfe779efb74a8bffb5b9d2fe3
Joelo246 <98349850+Joelo246@users.noreply.github.com>
2022-02-02 20:06:00 -0500

    Add files via upload

---
+++ number summing.py
@@ -0,0 +1,12 @@
+x = int
+y = int
+
+
+x = int(input('Please enter a number: '))
+y = int(input('Please enter a second number: '))
+
+sum = x + y
+    
+
+print("The sum of your numbers is:", sum)
+      
